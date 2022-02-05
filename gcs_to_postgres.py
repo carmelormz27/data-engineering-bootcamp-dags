468ec753b507d9e76a0ba545477e17678a9bbe03
Jose Carmelo Ramirez Beltran <jose.ramirez@wizeline.com>
2022-02-04 18:45:14 -0600

    updated logic of custom operator

--- gcs_to_postgres.py
+++ gcs_to_postgres.py
@@ -52,63 +52,65 @@ class GCSToPostgresTransfer(BaseOperator):
 
     csvFile = self.gcs.download(bucket_name = self.bucket, object_name = self.object).decode(encoding = "utf-8", errors = "ignore")
 
+    # self.conn = self.pg_hook.get_conn()
+    # self.cursor = self.conn.cursor()
+    # self.current_table = self.schema + '.' + self.table
+    # self.cursor.copy_from(csvFile, self.current_table, sep=",")
+    # self.conn.commit()
+
+
+
+    dataSchema = {
+                  'InvoiceNo': 'string',
+                  'StockCode': 'string',
+                  'Description': 'string',
+                  'Quantity': 'float64',
+                  'UnitPrice': 'float64',
+                  'CustomerID': 'float64',
+                  'Country': 'string'
+                 }
+    
+    date_cols = ['InvoiceDate']
+
+    # read csv file
+    df_purchases = pd.read_csv(io.StringIO(csvFile),
+      header=0,
+      delimiter=",",
+      quotechar='"',
+      # low_memory=False,
+      parse_dates=date_cols,
+      dtype=dataSchema
+      )
+    
+    self.log.info(df_purchases)
+    self.log.info(df_purchases.info())
+
+    df_purchases = df_purchases.replace(r"[\"]", r"'")
+    list_df_purchases = df_purchases.values.tolist()
+    list_df_purchases = [tuple(x) for x in list_df_purchases]
+
+    self.log.info(list_df_purchases)
+
+    list_target_fields = ['invoice_number', 
+                          'stock_code',
+                          'detail', 
+                          'quantity', 
+                          'invoice_date',
+                          'unit_price',
+                          'customer_id',
+                          'country'
+                          ]
+    
+    self.current_table = self.schema + '.' + self.table
+
+    self.pg_hook.insert_rows(self.current_table,
+                            list_df_purchases,
+                            target_fields = list_target_fields,
+                            commit_every = 1000,
+                            replace = False)
+
     self.conn = self.pg_hook.get_conn()
     self.cursor = self.conn.cursor()
-    self.current_table = self.schema + '.' + self.table
-    self.cursor.copy_from(csvFile, self.current_table, sep=",")
-    self.conn.commit()
-
-
-
-    # dataSchema = {
-    #               'InvoiceNo': 'string',
-    #               'StockCode': 'string',
-    #               'Description': 'string',
-    #               'Quantity': 'float64',
-    #               'UnitPrice': 'float64',
-    #               'CustomerID': 'float64',
-    #               'Country': 'string'
-    #              }
-    
-    # date_cols = ['InvoiceDate']
-
-    # # read csv file
-    # df_purchases = pd.read_csv(io.StringIO(csvFile),
-    #   header=0,
-    #   delimiter=",",
-    #   quotechar='"',
-    #   low_memory=False,
-    #   parse_dates=date_cols,
-    #   dtype=dataSchema
-    #   )
-    
-    # self.log.info(df_purchases)
-    # self.log.info(df_purchases.info())
-
-    # df_purchases = df_purchases.replace(r"[\"]", r"'")
-    # list_df_purchases = df_purchases.values.tolist()
-    # list_df_purchases = [tuple(x) for x in list_df_purchases]
-
-    # self.log.info(list_df_purchases)
-
-    # list_target_fields = ['invoice_number', 
-    #                       'stock_code',
-    #                       'detail', 
-    #                       'quantity', 
-    #                       'invoice_date',
-    #                       'unit_price',
-    #                       'customer_id',
-    #                       'country'
-    #                       ]
-    
-    # self.current_table = self.schema + '.' + self.table
-
-    # self.pg_hook.insert_rows(self.current_table,
-    #                         list_df_purchases,
-    #                         target_fields = list_target_fields,
-    #                         commit_every = 1000,
-    #                         replace = False)
-
     self.request = 'SELECT * FROM ' + self.current_table
     self.log.info(self.request)
 
